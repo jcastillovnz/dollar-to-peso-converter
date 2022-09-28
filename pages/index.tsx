175bc49d453d852c80522a499ad02449b36a50dd
Jose Castillo <jose.castilloext@externos-cl.cencosud.com>
2022-09-28 11:43:22 -0300

    feat: adding tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,7 +54,7 @@ const Home: NextPage = () => {
       <Head>
         <title>Conversor dolar a peso</title>
         <meta name="google-site-verification" content="jLy-jCzipmoaCT6-hXgJARacqDIXXhb_clgvi86fN1c" />
-        <meta name="description" content="Conversor dolar a peso, convertir dolar a pesos, precio de dolar argentina, conversor dolar a peso argentino" />
+        <meta name="description" content="Conversor dolar a peso, convertir dolar a pesos, precio de dolar argentina, conversor dolar a peso argentino, dólar a peso argentino blue" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
