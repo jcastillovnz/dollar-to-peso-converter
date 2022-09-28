7828d1fa7abadf81346c3890c221ed5ec851400b
Jose Castillo <jose.castilloext@externos-cl.cencosud.com>
2022-09-28 12:48:37 -0300

    feat adding changes

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,33 +54,34 @@ const Home: NextPage = () => {
       <Head>
         <title>Conversor dolar a peso</title>
         <meta name="google-site-verification" content="jLy-jCzipmoaCT6-hXgJARacqDIXXhb_clgvi86fN1c" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
         <meta name="description" content="Conversor dolar a peso, convertir dolar a pesos, precio de dolar argentina, conversor dolar a peso argentino, dólar a peso argentino blue" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
-        <h1>
+        <h1 style={{ fontSize: '1.5rem' }}>
           Conversor dolar a peso
         </h1>
-        <div className={styles.grid}>
-          COMPRA ARG {ARGBuy} = 1 USD | VENTA ARG {ARGSell} = 1 USD
+        <div className={styles.grid} style={{ fontSize: '0.9rem', flexDirection: 'column', justifyContent: 'space-between', alignContent: 'center', alignItems: 'center' }}>
+          <strong style={{ color: 'green' }}> COMPRA ARG {ARGBuy} = 1 USD {' '}</strong>  <strong style={{ color: 'red' }}>{' '} VENTA ARG {ARGSell} = 1 USD</strong>
         </div>
-        <div style={{ marginTop: '1rem' }}>
-          1 ARG =  {isNaN(1 / ARGSell) || (1 / ARGSell) === Infinity ? 0 : (1 / ARGSell)} USD
+        <div style={{ marginTop: '1rem', fontSize: '0.9rem' }}>
+          <strong>  1 ARG </strong> =  {isNaN(1 / ARGSell) || (1 / ARGSell) === Infinity ? 0 : (1 / ARGSell)} USD
         </div>
-        <div style={{ marginTop: '1rem' }}>
-          Ultima actualizacion: {dayjs(lastUpdate).format('DD/MM/YYYY h:mm a')}
+        <div style={{ marginTop: '1rem', fontSize: '0.9rem' }}>
+          <strong> Ultima actualizacion</strong>: {dayjs(lastUpdate).format('DD/MM/YYYY h:mm a')}
         </div>
         <div style={{ marginTop: '2rem' }}>
           {' '}
-          USD:
+          Usd:
           {' '}
           <input placeholder='dolares' value={usd} onChange={(e) => {
             const value = isNaN(parseFloat(e.target.value)) ? 0 : parseFloat(e.target.value)
             setDolar(value)
           }} />
           {' '}
-          ARG:
+          Arg:
           {' '}
           <input placeholder='pesos' value={arg} onChange={(e) => {
             const value = isNaN(parseFloat(e.target.value)) ? 0 : parseFloat(e.target.value)
@@ -88,8 +89,8 @@ const Home: NextPage = () => {
           }} />
 
         </div>
-      </main>
-    </div>
+      </main >
+    </div >
   )
 }
 
